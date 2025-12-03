b6c96c1b7f5cc0a83a3e11130992ed84b89de2d4
TasisAlwaz <yttasis.alwaz@gmail.com>
2025-12-03 12:50:04 +0530

    Use products.js file

--- javascript-amazon-project/Scripts/amazon.js
+++ javascript-amazon-project/Scripts/amazon.js
@@ -1,28 +1,45 @@
-const products = [{
-  image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating : {   //we made rating an object, because it had two parts, stars and count
-    stars : 4.5,
-    count : 87
-  },
-  priceCents : 1090 //since js has problem with decimals, we will write price in cents and then convert to dollars
-}, {
-  image : 'images/products/intermediate-composite-basketball.jpg',
-  name : 'Intermediate Size Basketball',
-  rating : {
-    stars : 4,
-    count : 127
-  },
-  priceCents : 2095
-}, {
-  image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating : {
-    stars : 4.5,
-    count : 56
-  },
-  priceCents : 799
-}];
+// const products = [{
+//   image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+//   name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+//   rating : {   //we made rating an object, because it had two parts, stars and count
+//     stars : 4.5,
+//     count : 87
+//   },
+//   priceCents : 1090 //since js has problem with decimals, we will write price in cents and then convert to dollars
+// }, {
+//   image : 'images/products/intermediate-composite-basketball.jpg',
+//   name : 'Intermediate Size Basketball',
+//   rating : {
+//     stars : 4,
+//     count : 127
+//   },
+//   priceCents : 2095
+// }, {
+//   image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+//   name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+//   rating : {
+//     stars : 4.5,
+//     count : 56
+//   },
+//   priceCents : 799
+// }, {
+//   image : 'images/products/black-2-slot-toaster.jpg',
+//   name : '2 Slot Toaster - Black',
+//   rating : {
+//     stars : 5.0,
+//     count : 2197
+//   }, 
+//   priceCents: 1899
+// }, {
+//   image : 'images/products/6-piece-white-dinner-plate-set.jpg',
+//   name : '6 Piece White Dinner Plate Set',
+//   rating : {
+//     stars : 4.0,
+//     count : 37
+//   },
+//   priceCents : 2067
+// }];
+
 
 let productsHTML = '';
 
@@ -80,6 +97,5 @@ products.forEach((product) => {
 
 });
 
-console.log(productsHTML);
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
