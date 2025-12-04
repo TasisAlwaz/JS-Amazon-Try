22b3e13095b2a3ee3442c23a3d6d965db9d857fb
TasisAlwaz <yttasis.alwaz@gmail.com>
2025-12-04 11:48:27 +0530

    Make add to Cart button Interactive

--- javascript-amazon-project/Scripts/amazon.js
+++ javascript-amazon-project/Scripts/amazon.js
@@ -1,44 +1,46 @@
-// const products = [{
-//   image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
-//   name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-//   rating : {   //we made rating an object, because it had two parts, stars and count
-//     stars : 4.5,
-//     count : 87
-//   },
-//   priceCents : 1090 //since js has problem with decimals, we will write price in cents and then convert to dollars
-// }, {
-//   image : 'images/products/intermediate-composite-basketball.jpg',
-//   name : 'Intermediate Size Basketball',
-//   rating : {
-//     stars : 4,
-//     count : 127
-//   },
-//   priceCents : 2095
-// }, {
-//   image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-//   name : 'Adults Plain Cotton T-Shirt - 2 Pack',
-//   rating : {
-//     stars : 4.5,
-//     count : 56
-//   },
-//   priceCents : 799
-// }, {
-//   image : 'images/products/black-2-slot-toaster.jpg',
-//   name : '2 Slot Toaster - Black',
-//   rating : {
-//     stars : 5.0,
-//     count : 2197
-//   }, 
-//   priceCents: 1899
-// }, {
-//   image : 'images/products/6-piece-white-dinner-plate-set.jpg',
-//   name : '6 Piece White Dinner Plate Set',
-//   rating : {
-//     stars : 4.0,
-//     count : 37
-//   },
-//   priceCents : 2067
-// }];
+/*
+const products = [{
+  image : 'images/products/athletic-cotton-socks-6-pairs.jpg',
+  name : 'Black and Gray Athletic Cotton Socks - 6 Pairs',
+  rating : {   //we made rating an object, because it had two parts, stars and count
+    stars : 4.5,
+    count : 87
+  },
+  priceCents : 1090 //since js has problem with decimals, we will write price in cents and then convert to dollars
+}, {
+  image : 'images/products/intermediate-composite-basketball.jpg',
+  name : 'Intermediate Size Basketball',
+  rating : {
+    stars : 4,
+    count : 127
+  },
+  priceCents : 2095
+}, {
+  image : 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+  name : 'Adults Plain Cotton T-Shirt - 2 Pack',
+  rating : {
+    stars : 4.5,
+    count : 56
+  },
+  priceCents : 799
+}, {
+  image : 'images/products/black-2-slot-toaster.jpg',
+  name : '2 Slot Toaster - Black',
+  rating : {
+    stars : 5.0,
+    count : 2197
+  }, 
+  priceCents: 1899
+}, {
+  image : 'images/products/6-piece-white-dinner-plate-set.jpg',
+  name : '6 Piece White Dinner Plate Set',
+  rating : {
+    stars : 4.0,
+    count : 37
+  },
+  priceCents : 2067
+}];
+*/
 
 
 let productsHTML = '';
@@ -89,7 +91,8 @@ products.forEach((product) => {
           Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id = "${product.id}">
           Add to Cart
         </button>
       </div>
@@ -97,5 +100,31 @@ products.forEach((product) => {
 
 });
 
-
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if(matchingItem) {
+        matchingItem.quantity += 1;
+      } else{
+        cart.push({
+        productId : productId,
+        quantity : 1
+        });
+      }
+      console.log(cart)
+
+      
+    });
+  });
